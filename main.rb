f4c8cb6bca95dd9089a333ea5479c651dfcf2184
Richard McGain <richard.mcgain@shopify.com>
2013-12-08 02:16:19 -0500

    fin.

--- main.rb
+++ main.rb
@@ -1,3 +1,50 @@
+require_relative 'queue'
+require 'haml'
+
 get '/' do
-  "Hello World"
+ engine = Haml::Engine.new(PAGE_HAML)
+ engine.render
 end
+
+post '/add' do
+  Queue.add_person request['email']
+  redirect back
+end
+
+post '/remove' do
+  Queue.remove_person request['email']
+  redirect back
+end
+
+def people
+  Queue.people
+end
+
+def current
+  Queue.current
+end
+
+PAGE_HAML = <<-EOS
+%html
+  !!! 5
+  %head
+    %title
+      Checkout Team Errbit Roster Thingy
+  %body
+    %h1
+      Checkout Team Errbit Roster Thingy
+    %div
+      Everyone:
+      = people
+    %div
+      Current cycle:
+      = current
+    %form{name: 'add', action: '/add', method: 'post'}
+      %label{for: 'add[email]'} Add someone
+      %input{type: 'text', name: 'email'}
+      %input{type: 'submit', value: 'Add', class: 'button'}
+    %form{name: 'remove', action: '/remove', method: 'post'}
+      %label{for: 'remove[email]'} remove someone
+      %input{type: 'text', name: 'email'}
+      %input{type: 'submit', value: 'remove', class: 'button'}
+EOS
--- queue.rb
+++ queue.rb
@@ -3,8 +3,12 @@ require 'redis'
 class Queue
   @redis = nil
   def self.redis
-    uri = URI.parse(ENV["REDISTOGO_URL"])
-    @redis ||= Redis.new( host: uri.host, port: uri.port, password: uri.password)
+    if ENV["REDISTOGO_URL"]
+      uri = URI.parse(ENV["REDISTOGO_URL"])
+      @redis ||= Redis.new( host: uri.host, port: uri.port, password: uri.password)
+    else
+      @redis ||= Redis.new
+    end
   end
 
   def self.next
@@ -13,9 +17,26 @@ class Queue
   end
 
   def self.reshuffle
-    people = redis.lrange('people', 0, -1).shuffle
+    people = self.people.shuffle
     recipient = people.shift
     redis.lpush 'current', people
     recipient
+  end
+
+  def self.people
+    redis.lrange('people', 0, -1)
+  end
+
+  def self.current
+    redis.lrange('current', 0, -1)
+  end
+
+  def self.add_person(email)
+    redis.rpush 'people', email
+  end
+
+  def self.remove_person(email)
+    redis.lrem 'current', 1, email
+    redis.lrem 'people', 1, email
   end
 end
