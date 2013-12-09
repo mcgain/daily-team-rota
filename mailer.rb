8ab9d717df8bd3f57c4272a640d5afd29b2ee024
Richard McGain <richard.mcgain@shopify.com>
2013-12-09 16:51:53 -0500

    update email

--- mailer.rb
+++ mailer.rb
@@ -25,13 +25,19 @@ end
 EMAIL_BODY = <<-EOS
 Good morning!
 
-Please keep an eye on errbit error reported today.
+Please keep an eye on errbit errors reported today.
 If an error should be fixed by the checkout team,
 raise an issue and tag it with 'AR Checkout Area'.
+
+https://logs.shopify.com:8000/en-US/app/search/CheckoutDashboard
+is an easier way to find errors than monitoring flowdock.
 
 If it looks important, you should probably tell people!
 
 If you cannot do this today, please ask someone else to cover for you.
+
+If it is a monday, or the day after the holiday,
+please have a look over the previous days too.
 
 Thanks,
 
