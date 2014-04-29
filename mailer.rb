c8cb8e6d34dd48ec36a8df1baf6a4512917c05ff
Richard McGain <richard.mcgain@shopify.com>
2014-04-29 17:55:17 +0000

    change the email to kitchen duty

--- mailer.rb
+++ mailer.rb
@@ -16,9 +16,9 @@ class Mailer
 
   def self.send_rota_email_to(recipient)
     Pony.mail(to: recipient,
-              cc: 'checkout@shopify.flowdock.com',
+              cc: 'montreal@shopify.flowdock.com',
               from: recipient,
-              subject: "#{recipient} is on errors duty today",
+              subject: "#{recipient} is on kitchen duty today",
               body: EMAIL_BODY)
   end
 end
@@ -26,14 +26,16 @@ end
 EMAIL_BODY = <<-EOS
 Good morning!
 
-It is your job to keep an eye on the dashboards today. If something goes wrong with
-gift cards or checkout, we need to be the first to know.
+You are on kitchen duty today. This involves:
+* Emptying the dishwasher if it has finished and putting the stuff in the sink in it.
+* Washing up the bain-marie, and anything else too big to fit in the dishwasher.
+* Putting the leftovers in the fridge.
+* Wipe the table if it is messy.
 
-Some useful dashboards are:
-https://logs.shopify.com:8000/en-US/app/search/CheckoutDashboard
-https://app.datadoghq.com/dash/dash/18553?live=true&from_ts=1397481759777&to_ts=1397485359777&tile_size=m
+It does not involve:
+* Cleaning up other people's mess.
 
 Thanks,
 
-The Errbit mailer roster system thingy
+The rest of the office.
 EOS
